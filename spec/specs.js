df314996150310ce7eb4de74eba0563352ec0303
Leah Sherrell & Diana Miller <student@epicodus.com>
2015-10-19 21:00:55 -0700

    add proper file to scripts.js and add comments to specs.js test spec-runner.html has a bug

--- spec/specs.js
+++ spec/specs.js
@@ -1,13 +1,16 @@
 describe('leapYear', function() {
-  it("is false for a year that is not divisible by 4", function() {
-    expect(leapYear(1999)).to.equal(false);
+  describe('leapYear', function() {
+    it("is false for a year that is not divisible by 4", function() {
+      expect(leapYear(1999)).to.equal(false);
+    });
+
+    it("is true for most years divisible by 4", function() {
+      expect(leapYear(2012)).to.equal(true);
+    });
+
+    it("is false for most years divisible by 100", function() {
+      expect(leapYear(1900)).to.equal(false);
+    });
   });
 
-  it("is true for most years divisible by 4", function() {
-    expect(leapYear(2012)).to.equal(true);
-  });
-
-  it("is false for most years divisible by 100", function() {
-    expect(leapYear (1900)).to.equal(false);
-  });
-});
+  //follow the monday lesson we have to add the %400 and fix bug in jquery ($(document))
